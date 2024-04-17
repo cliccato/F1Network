adc5452c90ce271a1de3b10933b6e68f651452e0
cliccato <cliccato@bigdi.cc>
2024-04-17 10:51:10 +0200

    added news page

---
+++ public/news.php
@@ -0,0 +1,33 @@
+<h1>Formula 1 News Feed</h1>
+
+<?php
+    require '../src/header.php';
+
+    $feed_url = "https://www.autosport.com/rss/feed/f1";
+
+    $xml = simplexml_load_file($feed_url);
+
+    if ($xml) {
+        $limit = 10;
+        $count = 0;
+        foreach ($xml->channel->item as $item) {
+            if ($count < $limit) {
+                $title = $item->title;
+                $description = $item->description;
+                
+                echo "<div class='news-card'>";
+                echo "<h2><a href='" . $item->link . "'>" . $title . "</a></h2>";
+                echo "<p>" . $description . "</p>";
+                echo "</div>";
+
+                $count++;
+            } else {
+                break;
+            }
+        }
+    } else {
+        echo "<p>Impossibile caricare il feed delle notizie di Formula 1.</p>";
+    }
+
+    require '../src/footer.php';
+?>
