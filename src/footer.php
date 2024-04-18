398ce8858a83754a65ad502178efcec8f31dbe3d
cliccato <cliccato@bigdi.cc>
2024-04-18 11:27:57 +0200

    update

--- src/footer.php
+++ src/footer.php
@@ -1,2 +1,11 @@
-<br><p>F1 Network (C) 2024</p>
+<footer>
+  <div class="footer-icons">
+    <a href="home.html"><i class="fas fa-home"></i></a>
+    <a href="search.html"><i class="fas fa-magnifying-glass"></i></a>
+    <a href="news.html"><i class="fas fa-newspaper"></i></a>
+  </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free/js/all.min.js"></script>
 </body>
+</html>
