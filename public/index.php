2d1286fee247f06604db020cc2c88873d509c233
loris2659y2 <128848819+loris2659y2@users.noreply.github.com>
2024-04-19 09:43:25 +0200

    ciao serghe

--- public/index.php
+++ public/index.php
@@ -3,17 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
     <title>F1 Network</title>
 </head>
 <body>
-    <form id="authForm" method="post">
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username" required><br>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password" required><br><br>
-        <input type="submit" value="Login" name="login">
-        <input type="submit" value="Register" name="register">
-    </form>
+    <div class="backgroud">
+        <form id="authForm" method="post">
+            <label for="username">Username:</label><br>
+            <input type="text" id="username" name="username" required><br>
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password" required><br><br>
+            <input type="submit" value="Login" name="login">
+            <input type="submit" value="Register" name="register">
+        </form>
+    </div>
 
     <script>
         var authForm = document.getElementById("authForm");
